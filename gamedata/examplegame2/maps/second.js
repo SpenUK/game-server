a0a3dda7bbd3e34c0056865d4f7f7fb916ecdfea
Spen Taylor <spen_@hotmail.co.uk>
2016-03-16 18:59:42 +0000

    Position is now working for portal moves, camera now tracks this properly as a result

--- gamedata/examplegame2/maps/second.js
+++ gamedata/examplegame2/maps/second.js
@@ -12,23 +12,21 @@ module.exports = {
 	tilesY: 10,
 	tileSize: 32,
 	tileMap: [
-		0,0,0,0,0,0,0,0,0,0,
-		0,1,1,1,1,1,1,1,1,0,
-		0,1,1,1,1,1,1,3,1,0,
-		0,1,1,1,3,1,1,1,1,0,
+		0,0,0,0,2,2,0,0,0,0,
 		0,1,1,1,1,1,1,1,1,0,
 		0,1,1,1,1,1,1,1,1,0,
 		0,1,1,1,1,1,1,1,1,0,
-		0,1,1,1,3,1,1,1,1,0,
-		0,1,1,1,1,1,1,3,1,0,
-		0,0,0,0,0,0,0,4,0,0
+		2,1,1,1,1,1,1,1,1,2,
+		2,1,1,1,1,1,1,1,1,2,
+		0,1,1,1,1,1,1,1,1,0,
+		0,1,1,1,1,1,1,1,1,0,
+		0,1,1,1,1,1,1,1,1,0,
+		0,0,0,0,2,2,0,0,0,0
 	],
 	tileTypes: [
+		{blocker: 1111, base: 30, props: 0, cover: 0},
 		{blocker: false, base: 1, props: 0, cover: 0},
-		{blocker: false, base: 2, props: 0, cover: 0},
-		{blocker: false, base: 0, props: 0, cover: 0},
-		{blocker: 1111, base: 0, props: 0, cover: 0},
-		{blocker: false, base: 20, props: 0, cover: 0, portal: 0}
+		{blocker: false, base: 1, props: 0, cover: 0, portal: 0}
 	],
 	eventTiles: [
 		{tile:6, target: {map: 'start', tile: 20 }},

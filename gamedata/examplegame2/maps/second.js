d8614c3695458286fb64c165c606335b4a17f266
Spen Taylor <spen_@hotmail.co.uk>
2016-03-16 09:12:01 +0000

    map change works, position not accounted for yet

--- gamedata/examplegame2/maps/second.js
+++ gamedata/examplegame2/maps/second.js
@@ -3,14 +3,14 @@
 module.exports = {
 	title: 'second',
 	tileSet: {
-		src: 'images/exampleTileSet.png',
-		tilesX: 4,
-		tilesY: 2,
-		tileSize: 50
+		src: 'images/rpg-tiles.png',
+		tilesX: 9,
+		tilesY: 6,
+		tileSize: 32
 	},
 	tilesX: 10,
 	tilesY: 10,
-	tileSize: 50,
+	tileSize: 32,
 	tileMap: [
 		0,0,0,0,0,0,0,0,0,0,
 		0,1,1,1,1,1,1,1,1,0,
@@ -21,16 +21,22 @@ module.exports = {
 		0,1,1,1,1,1,1,1,1,0,
 		0,1,1,1,3,1,1,1,1,0,
 		0,1,1,1,1,1,1,3,1,0,
-		0,0,0,0,0,0,0,0,0,0
+		0,0,0,0,0,0,0,4,0,0
 	],
 	tileTypes: [
-		{passable: false, base: 1, props: 0, cover: 0},
-		{passable: true, base: 2, props: 0, cover: 0},
-		{passable: false, base: 2, props: 5, cover: 0},
-		{passable: true, base: 2, props: 4, cover: 0}
+		{blocker: false, base: 1, props: 0, cover: 0},
+		{blocker: false, base: 2, props: 0, cover: 0},
+		{blocker: false, base: 0, props: 0, cover: 0},
+		{blocker: 1111, base: 0, props: 0, cover: 0},
+		{blocker: false, base: 20, props: 0, cover: 0, portal: 0}
 	],
 	eventTiles: [
 		{tile:6, target: {map: 'start', tile: 20 }},
 		{tile:49, target: {map: 'third', tile: 20 }}
+	],
+
+	portals: [
+		{location:31, map: 'start', x: 5, y: 5},
+		{location:32, map: 'third', x: 3, y: 7}
 	]
 };
--- gamedata/examplegame2/maps/start.js
+++ gamedata/examplegame2/maps/start.js
@@ -3,7 +3,7 @@
 module.exports = {
 	title: 'start',
 	tileSet: {
-		src: 'images/exampleTileSet.png',
+		src: 'images/rpg-tiles.png',
 		tilesX: 9,
 		tilesY: 6,
 		tileSize: 32
@@ -12,8 +12,8 @@ module.exports = {
 	tilesY: 20,
 	tileSize: 32,
 	tileMap: [
-		1, 15,28,29,28,29,28,29,28,29,28,29,28,29,12, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,15,28,29,28,29,30,31,28,29,12, 1,
-		15,23,36,37,36,37,36,37,36,37,36,37,36,37,27,12, 1, 1, 1, 1, 1, 1, 1, 1,15,23,36,37,36,37,38,39,36,37,27,12,
+		1, 15,28,29,28,29,30,31,28,29,28,29,28,29,12, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,15,28,29,28,29,30,31,28,29,12, 1,
+		15,23,36,37,36,37,38,39,36,37,36,37,36,37,27,12, 1, 1, 1, 1, 1, 1, 1, 1,15,23,36,37,36,37,38,39,36,37,27,12,
 		16,33, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,26,11, 1, 1, 1, 1, 1, 1, 1,15,23,33, 1, 1, 1, 1, 1, 1, 1, 1,26,11,
 		17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10, 1, 1, 1, 1, 1, 1,15,23,33, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,
 		17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10, 1, 1, 1, 1, 1, 1,16,33, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,
@@ -31,66 +31,71 @@ module.exports = {
 		17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,
 		17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,
 		17, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,10,
-		13, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4,22
+		13, 5, 6, 7, 8, 5, 4, 5, 4, 8, 3, 5, 4, 5, 4, 3, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4, 5, 4,22
 	],
 	tileTypes: [
-		{blocker: 1111, base: 0, props: 0, cover: 0},
-		{blocker:    0, base: 1, props: 0, cover: 0},
-		{blocker: 1111, base: 10, props: 0, cover: 0},
-		{blocker: 1000, base: 11, props: 0, cover: 0},
-		{blocker: 1000, base: 12, props: 0, cover: 0},
+		{blocker: 1111, base: 0, props: 0, cover: 0, event: false},
+		{blocker:    0, base: 1, props: 0, cover: 0, event: false},
+		{blocker: 1111, base: 10, props: 0, cover: 0, event: false},
+		{blocker: 1000, base: 11, props: 0, cover: 0, event: false},
+		{blocker: 1000, base: 12, props: 0, cover: 0, event: false},
 
-		{blocker: 1000, base: 13, props: 0, cover: 0},
-		{blocker: 1000, base: 14, props: 0, cover: 0},
-		{blocker: 1000, base: 15, props: 0, cover: 0},
-		{blocker: 1000, base: 16, props: 0, cover: 0},
-		{blocker: 1111, base: 17, props: 0, cover: 0},
-
-
-		{blocker: 1111, base: 19, props: 0, cover: 0},
-		{blocker: 1111, base: 20, props: 0, cover: 0},
-		{blocker:    0, base: 21, props: 0, cover: 0},
-		{blocker: 1000, base: 22, props: 0, cover: 0},
-		{blocker: 1000, base: 23, props: 0, cover: 0},
-
-		{blocker: 1111, base: 24, props: 0, cover: 0},
-		{blocker: 1111, base: 25, props: 0, cover: 0},
-		{blocker: 1111, base: 26, props: 0, cover: 0},
-		{blocker: 1111, base: 28, props: 0, cover: 0},
-		{blocker:    0, base: 29, props: 0, cover: 0},
+		{blocker: 1000, base: 13, props: 0, cover: 0, event: false},
+		{blocker: 1000, base: 14, props: 0, cover: 0, event: false},
+		{blocker: 1000, base: 15, props: 0, cover: 0, event: false},
+		{blocker: 1000, base: 16, props: 0, cover: 0, event: false},
+		{blocker: 1111, base: 17, props: 0, cover: 0, event: false},
 
 
-		{blocker: 1111, base: 30, props: 0, cover: 0},
-		{blocker: 1000, base: 31, props: 0, cover: 0},
-		{blocker: 1000, base: 32, props: 0, cover: 0},
-		{blocker: 1111, base: 33, props: 0, cover: 0},
-		{blocker:    0, base: 34, props: 0, cover: 0},
+		{blocker: 1111, base: 19, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 20, props: 0, cover: 0, portal: false},
+		{blocker:    0, base: 21, props: 0, cover: 0, portal: false},
+		{blocker: 1000, base: 22, props: 0, cover: 0, portal: false},
+		{blocker: 1000, base: 23, props: 0, cover: 0, portal: false},
 
-		{blocker: 1111, base: 35, props: 0, cover: 0},
-		{blocker: 1111, base: 37, props: 0, cover: 0},
-		{blocker: 1111, base: 38, props: 0, cover: 0},
-		{blocker: 1111, base: 39, props: 0, cover: 0},
-		{blocker: 1111, base: 40, props: 0, cover: 0},
+		{blocker: 1111, base: 24, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 25, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 26, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 28, props: 0, cover: 0, portal: false},
+		{blocker:    0, base: 29, props: 0, cover: 0, portal: false},
 
 
-		{blocker: 1111, base: 41, props: 0, cover: 0},
-		{blocker: 1111, base: 42, props: 0, cover: 0},
-		{blocker: 1111, base: 43, props: 0, cover: 0},
-		{blocker: 1111, base: 44, props: 0, cover: 0},
-		{blocker:    0, base: 46, props: 0, cover: 0},
+		{blocker: 1111, base: 30, props: 0, cover: 0, portal: false},
+		{blocker: 1000, base: 31, props: 0, cover: 0, portal:     0},
+		{blocker: 1000, base: 32, props: 0, cover: 0, portal:     1},
+		{blocker: 1111, base: 33, props: 0, cover: 0, portal: false},
+		{blocker:    0, base: 34, props: 0, cover: 0, portal: false},
 
-		{blocker: 1111, base: 47, props: 0, cover: 0},
-		{blocker: 1111, base: 48, props: 0, cover: 0},
-		{blocker: 1111, base: 49, props: 0, cover: 0},
-		{blocker: 1010, base: 50, props: 0, cover: 0},
-		{blocker: 1001, base: 51, props: 0, cover: 0},
+		{blocker: 1111, base: 35, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 37, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 38, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 39, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 40, props: 0, cover: 0, portal: false},
 
-		{blocker: 1111, base: 52, props: 0, cover: 0},
-		{blocker:    0, base: 53, props: 0, cover: 0}
+
+		{blocker: 1111, base: 41, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 42, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 43, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 44, props: 0, cover: 0, portal: false},
+		{blocker:    0, base: 46, props: 0, cover: 0, portal: false},
+
+		{blocker: 1111, base: 47, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 48, props: 0, cover: 0, portal: false},
+		{blocker: 1111, base: 49, props: 0, cover: 0, portal: false},
+		{blocker: 1010, base: 50, props: 0, cover: 0, portal:     0},
+		{blocker: 1001, base: 51, props: 0, cover: 0, portal:     0},
+
+		{blocker: 1111, base: 52, props: 0, cover: 0, portal: false},
+		{blocker:    0, base: 53, props: 0, cover: 0, portal: false}
 	],
+
 	eventTiles: [
-		{location:6, target: {map: 'second', tile: 20 }},
-		{location:22, target: {map: 'third', tile: 20 }},
-		{location:49, target: {map: 'third', tile: 20 }}
+		{location:31, target: {map: 'second', tile: 20 }},
+		{location:32, target: {map: 'third', tile: 20 }}
+	],
+
+	portals: [
+		{location:31, map: 'second', x: 4, y: 9},
+		{location:32, map: 'third', x: 9, y: 4}
 	]
 };
